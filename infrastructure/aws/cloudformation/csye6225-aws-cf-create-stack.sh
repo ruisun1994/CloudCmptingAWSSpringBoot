bc628f795b227dd74db6e8a1b19b5329ea5ee455
Rui <373000335@qq.com>
2018-02-10 20:32:31 -0500

    add exit gracefully if create or terminate stack failed

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -3,7 +3,35 @@
 echo "-------Enter The Stack Name:"
 read stackname
 
-echo "-------Start to build the cloudformation:"
-aws cloudformation create-stack --stack-name "$stackname-csye6225-stack" --template-body file://csye6225-cf-networking.json --parameters ParameterKey=STACK_NAME,ParameterValue=$stackname&&
+echo
+echo "-------Test if the Stack existed:"
+stack_Id=$(aws cloudformation describe-stacks --stack-name "$stackname" --query 'Stacks[0].StackId' --output text)
+echo $stack_Id
+if 
+	[ "$stack_Id" != "" ]; then
+	echo "Stack has already existed, terminate first!"
+	exit 0
+fi
 
-echo "-------Stack created!"
+echo "-------Start to build the cloudformation:"
+aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-networking.json
+
+echo "-------Check if the cloudformation has been done sucessfully!"
+stackStatus=$(aws cloudformation describe-stacks --stack-name $stackname --query 'Stacks[0].StackStatus' --output text)
+
+while [[ "$stackStatus" != "CREATE_COMPLETE" && "$stackStatus" != "ROLLBACK_COMPLETE" ]]
+do
+	stackStatus=$(aws cloudformation describe-stacks --stack-name $stackname --query 'Stacks[0].StackStatus' --output text)
+	echo "Please wait a moment!"
+	echo $stackStatus
+	sleep 3
+done
+
+if [ "$stackStatus" == "CREATE_COMPLETE" ]; then
+	#statements
+	echo "cloudformation sucessfully!"
+	exit 0
+else
+	echo "failed"
+	exit 0
+fi
