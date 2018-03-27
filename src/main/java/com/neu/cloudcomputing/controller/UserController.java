31a99c296db40fe91bd9db939b0ac8bd1f114062
Rui <373000335@qq.com>
2018-03-27 15:16:33 -0400

    ui

--- src/main/java/com/neu/cloudcomputing/controller/UserController.java
+++ src/main/java/com/neu/cloudcomputing/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.neu.cloudcomputing.controller;
 
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.model.CreateTopicResult;
+import com.amazonaws.services.sns.model.SubscribeRequest;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+import com.amazonaws.services.sns.model.DeleteTopicRequest;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
@@ -276,6 +286,22 @@ public class UserController {
         }
         session.invalidate();
         return new ModelAndView("index");
+    }
+
+    @GetMapping(value = "/forgetPassword")
+    public ModelAndView forgetPassword(){
+        //create a new SNS client and set endpoint
+        AmazonSNSClient snsClient = new AmazonSNSClient(new ClasspathPropertiesFileCredentialsProvider());
+        snsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
+        //create a new SNS topic
+        CreateTopicRequest createTopicRequest = new CreateTopicRequest("MyNewTopic");
+        CreateTopicResult createTopicResult = snsClient.createTopic(createTopicRequest);
+        //print TopicArn
+        System.out.println(createTopicResult);
+        //get request id for CreateTopicRequest from SNS metadata
+        System.out.println("CreateTopicRequest - " + snsClient.getCachedResponseMetadata(createTopicRequest));
+
+        return new ModelAndView("profile");
     }
 
 }
